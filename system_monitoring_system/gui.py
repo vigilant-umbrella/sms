5fb99b66af3e9c328ee788ef761e42cd7b53680f
Ashok Arora <arora8715@gmail.com>
2021-04-12 00:11:01 +0400

    Adds change email and current email

--- system_monitoring_system/gui.py
+++ system_monitoring_system/gui.py
@@ -1,62 +1,115 @@
 import PySimpleGUI as sg
 
 
-def layout(row, col):
-    main_menu = [[sg.Text('Its the main menu')], [sg.Button(
-        'Generate report'), sg.Button('Send report as email',)]]
-    cpu_menu = [[sg.Text('Its the cpu menu')], [sg.Button(
-        'Generate report'), sg.Button('Send report as email')]]
-    memory_menu = [[sg.Text('Its the memory menu')], [sg.Button(
-        'Generate report'), sg.Button('Send report as email')]]
-    process_menu = [[sg.Text('Its the process menu')], [sg.Button(
-        'Generate report'), sg.Button('Send report as email')]]
-    storage_menu = [[sg.Text('Its the storage menu')], [sg.Button(
-        'Generate report'), sg.Button('Send report as email')]]
-    network_menu = [[sg.Text('Its the network menu')], [sg.Button(
-        'Generate report'), sg.Button('Send report as email')]]
-    misc_menu = [[sg.Text('Its the miscellanous menu')], [sg.Button(
-        'Generate report'), sg.Button('Send report as email')]]
-    settings = [[sg.Text('Its the settings menu')], [sg.Button('Set notification limit'), sg.Button(
-        'Change email'), sg.Button('Add email'), sg.Button('Change password')]]
+def layout(settings):
 
-    layout = [[sg.T("")],
-              [sg.TabGroup(
-                  [
-                      [sg.Tab('Main Menu', main_menu), sg.Tab('CPU Menu', cpu_menu), sg.Tab('Memory Menu', memory_menu),
-                       sg.Tab('Process Menu', process_menu), sg.Tab(
-                          'Storage Menu', storage_menu), sg.Tab('Network Menu', network_menu),
-                       sg.Tab('Misc Menu', misc_menu), sg.Tab(
-                          'Settings Menu', settings),
-                       ]
-                  ], border_width=5, pad=100, enable_events=True
-              )
+    main_menu = [
+        [sg.Text("Its the main menu")],
+        [
+            sg.Button("Generate report"),
+            sg.Button(
+                "Send report as email",
+            ),
+        ],
     ]
+    cpu_menu = [
+        [sg.Text("Its the cpu menu")],
+        [sg.Button("Generate report"), sg.Button("Send report as email")],
+    ]
+    memory_menu = [
+        [sg.Text("Its the memory menu")],
+        [sg.Button("Generate report"), sg.Button("Send report as email")],
+    ]
+    process_menu = [
+        [sg.Text("Its the process menu")],
+        [sg.Button("Generate report"), sg.Button("Send report as email")],
+    ]
+    storage_menu = [
+        [sg.Text("Its the storage menu")],
+        [sg.Button("Generate report"), sg.Button("Send report as email")],
+    ]
+    network_menu = [
+        [sg.Text("Its the network menu")],
+        [sg.Button("Generate report"), sg.Button("Send report as email")],
+    ]
+    misc_menu = [
+        [sg.Text("Its the miscellanous menu")],
+        [sg.Button("Generate report"), sg.Button("Send report as email")],
     ]
 
-    window = sg.Window('System Monitoring System', layout,
-                       size=(900, 700), element_justification='c')
+    email = settings.get("email", "None")
+    settings = [
+        [sg.Text("Its the settings menu")],
+        [sg.Text("Current email: "), sg.Text(
+            email, key="-email-", size=(200, 1))],
+        [
+            sg.Button("Set notification limit"),
+            sg.Button("Change email"),
+            sg.Button("Add email"),
+            sg.Button("Change password"),
+        ],
+    ]
+
+    layout = [
+        [sg.T("")],
+        [
+            sg.TabGroup(
+                [
+                    [
+                        sg.Tab("Main Menu", main_menu),
+                        sg.Tab("CPU Menu", cpu_menu),
+                        sg.Tab("Memory Menu", memory_menu),
+                        sg.Tab("Process Menu", process_menu),
+                        sg.Tab("Storage Menu", storage_menu),
+                        sg.Tab("Network Menu", network_menu),
+                        sg.Tab("Misc Menu", misc_menu),
+                        sg.Tab("Settings Menu", settings),
+                    ]
+                ],
+                border_width=5,
+                pad=100,
+                enable_events=True,
+            )
+        ],
+    ]
+
+    window = sg.Window(
+        "System Monitoring System", layout, size=(900, 700), element_justification="c"
+    )
     # window["abc"].Widget.configure(highlightcolor='red', highlightthickness=2)
     return window
 
 
 def authenticate(settings):
     auth = False
-    layout = [[sg.Text('Enter Administrator password:')], [sg.InputText()], [
-        sg.Button('Authenticate'), sg.Button('Exit')]]
-    window = sg.Window('Authenticate as administrator',
-                       layout, finalize=True, modal=True)
+    layout = [
+        [sg.Text("Enter Administrator password:")],
+        [sg.InputText()],
+        [sg.Button("Authenticate"), sg.Button("Exit")],
+    ]
+    window = sg.Window(
+        "Authenticate as administrator", layout, finalize=True, modal=True
+    )
     while True:
         event, values = window.read()
-        if (values[0] != settings.get('password', None)) or event == "Exit" or event == sg.WIN_CLOSED:
-            sg.popup('Authentication error',
-                     'Error in Authentication, taking back to main menu.')
+
+        if (values[0] != settings.get("password", None)) or event in (
+            "Exit",
+            sg.WIN_CLOSED,
+        ):
+            sg.popup(
+                "Error in Authentication, taking back to main menu.",
+                title="Authentication error",
+            )
             auth = False
-            # print('lulu')
+            # print("lulu")
             break
 
-        elif (values[0] == settings.get('password', None)):
-            sg.popup('Authentication successful',
-                     'You can now access the settings menu.')
+        if values[0] == settings.get("password", None):
+            sg.popup(
+                "You can now access the settings menu.",
+                title="Authentication successful",
+            )
             auth = True
             # print('aulelele')
             break
@@ -65,45 +118,69 @@ def authenticate(settings):
     return auth
 
 
-def change_password(settings):
-    layout = [[sg.Text('Enter new password:')], [sg.InputText()], [
-        sg.Button('Apply'), sg.Button('Exit')]]
-    window = sg.Window('Change Password', layout, finalize=True, modal=True)
+def change_email(settings):
+    layout = [
+        [sg.Text("Enter new email:")],
+        [sg.InputText()],
+        [sg.Button("Apply"), sg.Button("Exit")],
+    ]
+    window = sg.Window("Change email", layout, finalize=True, modal=True)
     while True:
         event, values = window.read()
-        settings.set('password', values[0])
-        if event == "Exit" or event == sg.WIN_CLOSED:
-            sg.popup('Authentication error',
-                     'Error in Authentication, taking back to main menu.')
+        settings.set("email", values[0])
+        if event in ("Exit", sg.WIN_CLOSED):
             break
 
         elif event == "Apply":
-            sg.popup('Password changed successfully')
+            sg.popup("Email changed successfully")
+            break
+    window.close()
+
+
+def change_password(settings):
+    layout = [
+        [sg.Text("Enter new password:")],
+        [sg.InputText()],
+        [sg.Button("Apply"), sg.Button("Exit")],
+    ]
+    window = sg.Window("Change Password", layout, finalize=True, modal=True)
+    while True:
+        event, values = window.read()
+        settings.set("password", values[0])
+        if event in ("Exit", sg.WIN_CLOSED):
+            break
+
+        elif event == "Apply":
+            sg.popup("Password changed successfully")
             break
 
     window.close()
 
 
 def main():
-    sg.theme('SandyBeach')
+    sg.theme("SandyBeach")
     auth = False
-    settings = sg.UserSettings(filename='./settings.json')
-
-    window = layout(10, 10)
+    settings = sg.UserSettings(filename="./settings.json")
+    print(settings.full_filename)
+    window = layout(settings)
 
     while True:
         event, values = window.read()
         print(event, values)
         if event == sg.WIN_CLOSED:
             break
-        elif values[0] == 'Settings Menu':
-            if auth == False:
+        if values[0] == "Settings Menu":
+            if not auth:
                 auth = authenticate(settings)
-                if auth == False:
+                if not auth:
                     window.Element("Main Menu").select()
-        if event == 'Change password':
+        if event == "Change password":
             change_password(settings)
-            # print('lalala')
+        elif event == "Change email":
+            change_email(settings)
+            window['-email-'].update(settings.get("email", "None"))
+        # window.refresh()
+        # print('lalala')
     window.close()
 
 
