72433602858a0f6258d46624268848df0cc36a8b
Aniket Sharma <aniketsharma00411@gmail.com>
2021-04-23 13:15:07 +0530

    Completes cli calling

--- sms.py
+++
@@ -1,4 +0,0 @@
-import system_monitoring_system.__main__ as main
-
-if __name__ == '__main__':
-    main.main()
--- system_monitoring_system/__main__.py
+++ system_monitoring_system/__main__.py
@@ -1,5 +1,35 @@
 import cli
+import fire
+import gui
+import monitoring
 
 
-def main():
-    cli.call(['-S'])
+if __name__ == '__main__':
+    commands = {
+        '--summary': cli.summary,
+        '-S': cli.summary,
+        '--cpu': cli.cpu,
+        '-c': cli.cpu,
+        '--memory': cli.memory,
+        '-M': cli.memory,
+        '--process': cli.process,
+        '-p': cli.process,
+        '--storage': cli.storage,
+        '-s': cli.storage,
+        '--network': cli.network,
+        '-n': cli.network,
+        '--misc': cli.misc,
+        '-m': cli.misc,
+        '--show-limit': cli.show_limit,
+        '--update-limit': cli.update_limit,
+        '--show-email': cli.show_email,
+        '--update-email add': cli.add_email,
+        '--update-email remove': cli.remove_email,
+        '--update-email modify': cli.modify_email,
+        '--update-password': cli.update_password,
+        '--start-monitoring': monitoring.start,
+        '--gui': gui.main,
+        '-g': gui.main
+    }
+
+    fire.Fire(commands)
